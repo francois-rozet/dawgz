f75d1232deb712c8d071bc604ebf950cef6ec85e
Joeri Hermans <joeri@peinser.com>
2022-01-11 23:16:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ setuptools.setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10'
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     python_requires='>=3.8',
 )
